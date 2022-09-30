a9a549351ba5bd629fcca1f8d03e51919bd4ca90
Robin Mauritz <robinmauritzrm@gmail.com>
2022-09-30 15:36:22 +0700

    feat: Switch from Maven to Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+}
+
+group = "com.nawadata"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.seleniumhq.selenium:selenium-chrome-driver:3.141.59")
+    implementation("org.seleniumhq.selenium:selenium-java:3.141.59")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.register<Jar>("uberJar") {
+    archiveClassifier.set("full")
+
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "AutalonDriver"
+includeFlat("Tester")
