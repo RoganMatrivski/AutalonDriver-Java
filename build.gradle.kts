3f52c82d0f8151e083e74c4783b4438147ccf1d5
Robin Mauritz <robinmauritzrm@gmail.com>
2022-11-02 12:32:11 +0700

    ci: fix build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    `maven-publish`
+
     kotlin("jvm") version "1.7.10"
 }
-
-group = "com.nawadata"
-version = "0.1.2"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
@@ -40,4 +39,24 @@ tasks.register<Jar>("publishJAR") {
     from({
         configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
     })
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("default") {
+            from(components["java"])
+            // Include any other artifacts here, like javadocs
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/RoganMatrivski/AutalonDriver-Java")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
